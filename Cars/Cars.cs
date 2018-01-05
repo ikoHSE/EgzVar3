5cb14a9bb0c8c4ec42def3d3b2842fb441b03fc7
ilyakooo0 <ilyakooo0@gmail.com>
2018-01-05 15:22:34 +0530

    Initial commit

---
+++ Cars/Cars.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using static System.Console;
+
+namespace Cars {
+
+    /// <summary>
+    /// A generic car.
+    /// </summary>
+    public abstract class Car {
+
+        /// <summary>
+        /// A random number generator.
+        /// </summary>
+        protected static Random rand = new Random();
+
+        /// <summary>
+        /// The field storing x.
+        /// </summary>
+        private int _x = 1;
+
+        /// <summary>
+        /// The x position of the car. Must be >= 1.
+        /// </summary>
+        /// <value>x.</value>
+        public int X {
+            get => _x;
+            set {
+                if (value < 1) {
+                    throw new Exception("X must be larger than 1");
+                }
+                _x = value;
+            }
+        }
+
+        /// <summary>
+        /// The name of the car.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The symbol represnting the car on screen.
+        /// </summary>
+        /// <value>The symbol.</value>
+        public abstract Char Symbol { get; }
+
+        /// <summary>
+        /// Moves the car a set random number of units.
+        /// </summary>
+        public abstract void Step();
+
+        /// <summary>
+        /// Prints the car to the console.
+        /// </summary>
+        /// <param name="width">The width of the road.</param>
+        public void PrintOnMap(int width) {
+            if (width < 0) {
+                throw new Exception("Width can not be negative");
+            }
+            var map = $"{Name}:\t [";
+            for (var i = 0; i < width; i++) {
+                if (i == X - 1) {
+                    map += Symbol;
+                } else {
+                    map += '-';
+                }
+            }
+            map += ']';
+            WriteLine(map);
+        }
+    }
+
+    /// <summary>
+    /// a speed car.
+    /// </summary>
+    public class SpeedCar: Car {
+        /// <summary>
+        /// The symbol represnting the car on screen.
+        /// </summary>
+        /// <value>The symbol.</value>
+        public override char Symbol => '>';
+
+        /// <summary>
+        /// Moves the car form 3 to 5 units in the positive x direction.
+        /// </summary>
+        public override void Step() {
+            X += rand.Next(3, 6);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Cars.SpeedCar"/> class.
+        /// </summary>
+        /// <param name="name">Name.</param>
+        public SpeedCar(string name) {
+            this.Name = name;
+        }
+    }
+
+    /// <summary>
+    /// A slow car.
+    /// </summary>
+    public class SlowCar: Car {
+        /// <summary>
+        /// The symbol represnting the car on screen.
+        /// </summary>
+        /// <value>The symbol.</value>
+        public override char Symbol => 'o';
+
+        /// <summary>
+        /// Moves the car form 0 to 2 units in the positive x direction.
+        /// </summary>
+        public override void Step() {
+            X += rand.Next(0, 3);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Cars.SlowCar"/> class.
+        /// </summary>
+        /// <param name="name">Name.</param>
+        public SlowCar(string name) {
+            this.Name = name;
+        }
+    }
+}
---
+++ EgzVar3/Program.cs
@@ -0,0 +1,79 @@
+﻿
+/*
+ФИО: Костюченко Илья Игоревич
+Группа: БПИ 171-1
+Дата: 19.12.17
+Вариант: 3
+*/
+
+using System;
+using static Iko.Iko;
+using Cars;
+using System.Linq;
+
+namespace EgzVar3 {
+    class MainClass {
+        private static Random rand = new Random();
+        public static void Main() {
+            Repeat(() => {
+                var width = rand.Next(10, 31);
+                var cars = new Car[5];
+                for (var i = 0; i < cars.Length; i++) {
+                    switch (rand.Next(0, 2)) {
+                    case 0:
+                        cars[i] = new SpeedCar(GenerateName(6));
+                        break;
+                    case 1:
+                        cars[i] = new SlowCar(GenerateName(6));
+                        break;
+                    }
+                }
+
+                foreach (var c in cars) {
+                    c.PrintOnMap(width);
+                }
+
+                Console.WriteLine();
+                Console.Write("Press sace to move cars.");
+
+                while (Console.ReadKey().Key == ConsoleKey.Spacebar) {
+                    Console.WriteLine();
+                    foreach (var c in cars) {
+                        c.Step();
+                        c.PrintOnMap(width);
+                    }
+
+                    // Можно без Linq с флагом.
+
+                    if (cars.Select(c => c.X >= width).Aggregate(false, (arg1, arg2) => arg1 || arg2)) {
+                        Console.WriteLine("A car has reached the end of the road!");
+                        break;
+                    }
+
+                    Console.WriteLine();
+                    Console.Write("Press sace to move cars.");
+                }
+
+            });
+        }
+
+        /// <summary>
+        /// Generates a random name with the given number of symbols.
+        /// </summary>
+        /// <returns>The name.</returns>
+        /// <param name="width">Width.</param>
+        private static string GenerateName(int width) {
+            char minChar = 'a';
+            char maxChar = 'z';
+            var name = "";
+            for (var i = 0; i < width; i++) {
+                char nextChar = (char) (minChar + rand.Next(0, maxChar - minChar + 1));
+                if (i == 0) {
+                    nextChar = Char.ToUpper(nextChar);
+                }
+                name += nextChar;
+            }
+            return name;
+        }
+    }
+}
